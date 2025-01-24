e575855621e806c5c844c78bb257ccb5358f1456
Gyanvendra Singh <gyanvendras2004@gmail.com>
2025-01-24 17:04:02 +0530

    postgres setup done

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,25 @@
+import Image, { type ImageProps } from "next/image";
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+   <div className="text-3xl bg-yellow-200">hello</div>
+  );
+}
